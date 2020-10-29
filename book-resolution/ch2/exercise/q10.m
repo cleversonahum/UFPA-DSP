d4b2b3fd22372ba1542c5f640f4c9e0c9b8423ad
Cleverson Nahum <cleversonahum@gmail.com>
2020-10-28 21:58:54 -0300

    adding resolution to 2 exercise of chapter 2

---
+++ book-resolution/ch2/exercise/q10.m
@@ -0,0 +1,3 @@
+N = 3
+A = (1/sqrt(N))*dftmtx(3)
+A_i = conj(A)
---
+++ book-resolution/ch2/exercise/q3.m
@@ -0,0 +1,12 @@
+C = [120 140 139; 124 220 120; 114 130 122]
+
+disp('DCT')
+A = dctmtx(3)
+T = A*C'
+C_dct = A*T'
+
+clear A, T
+disp('DFT')
+A = dftmtx(3)
+T = A*C'
+C_dft = A*T'
